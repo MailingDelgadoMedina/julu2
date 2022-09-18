07a66579a3faa9b301f013f80b905c924089db5f
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-09-18 01:22:10 -0400

    created HeaderItem component added props by destructuring

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div>
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Home" Icon={HomeIcon} />
+      </div>
+      <Image
+        className="object-contain"
+        src="https://res.cloudinary.com/programandoconmei/image/upload/v1663476468/JULUtransparent_kzds8o.svg"
+        width={200}
+        height={100}
+      />
+      Header
+    </div>
+  );
 }
 
 export default Header;

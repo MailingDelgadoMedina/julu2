7c5face053d34d7ad5a01f60149ef691f7a432fa
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-09-18 23:10:55 -0400

    finished second nav and created templates results

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+
 export default function Home() {
   return (
     <div>
@@ -17,6 +19,24 @@ export default function Home() {
       <Header />
 
       <Nav />
+
+      <Results />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }

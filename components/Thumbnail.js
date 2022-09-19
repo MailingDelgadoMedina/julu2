667f0fdcf9f7f9e8de87fe14dbad671fb1e34ce3
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-09-19 02:56:08 -0400

    fixed ref

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import { forwardRef } from "react";
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 const Thumbnail = forwardRef(({ result }, ref) => {

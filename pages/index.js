9769beb6db59acc8e63905bc14edf7ea57968a0d
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-09-18 23:17:53 -0400

    finished fetch

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import Image from "next/image";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
-
-export default function Home() {
+import requests from "../utils/requests";
+export default function Home(props) {
+  console.log(props);
   return (
     <div>
       <Head>
@@ -30,7 +31,7 @@ export async function getServerSideProps(context) {
 
   const request = await fetch(
     `https://api.themoviedb.org/3${
-      requests[genre]?.url || requests.fetchTrending.url
+      requests[genre].url || requests.fetchTrending.url
     }`
   ).then((res) => res.json());
 
